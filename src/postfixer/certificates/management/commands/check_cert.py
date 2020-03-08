64ba84614b9d4facfa39003a8e9f47b69cf40170
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-08 19:55:56 +0100

    :alembic: Look up MX hosts and IP adresses

--- src/postfixer/certificates/management/commands/check_cert.py
+++ src/postfixer/certificates/management/commands/check_cert.py
@@ -3,9 +3,11 @@ import os
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from ...utils import extract_domains
+from ...utils import extract_domains, get_relevant_mx_hosts
 
 TEST_CERT = os.path.join(settings.BASE_DIR, "certs", "fullchain.pem",)
+
+HOST = "pi-modelling.nl"
 
 
 class Command(BaseCommand):
@@ -14,3 +16,8 @@ class Command(BaseCommand):
         self.stdout.write("Domains: ")
         for domain in domains:
             self.stdout.write(f"- {domain}")
+
+        hosts = get_relevant_mx_hosts(HOST)
+        self.stdout.write(f"Relvant MX hosts for {HOST}:")
+        for host in hosts:
+            self.stdout.write(f"- {host[0]}: {host[1]}")
--- src/postfixer/certificates/utils.py
+++ src/postfixer/certificates/utils.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Tuple
 
+import dns.resolver
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 
@@ -11,3 +12,16 @@ def extract_domains(certificate_file: str) -> List[str]:
     ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
     subj_alt_names = ext.value.get_values_for_type(x509.DNSName)
     return subj_alt_names
+
+
+def get_relevant_mx_hosts(domain: str) -> List[Tuple[str, List[str]]]:
+    answers = dns.resolver.query(domain, "MX")
+
+    mx_servers = [answer.exchange.to_text() for answer in answers]
+
+    resolved = []
+    for server in mx_servers:
+        ips = [answer.to_text() for answer in dns.resolver.query(server, "A")]
+        resolved.append((server, ips))
+
+    return resolved
