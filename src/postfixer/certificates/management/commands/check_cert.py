741b993f00820329b3a510818de3f5ab056449d4
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-09 19:11:47 +0100

    :sparkles: Implement certificate + DNS inspection

--- src/postfixer/certificates/management/commands/check_cert.py
+++ src/postfixer/certificates/management/commands/check_cert.py
@@ -3,21 +3,19 @@ import os
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from ...utils import extract_domains, get_relevant_mx_hosts
+from ...utils import get_certificate_domains
 
 TEST_CERT = os.path.join(settings.BASE_DIR, "certs", "fullchain.pem",)
 
 HOST = "pi-modelling.nl"
 
+MAIL_HOSTS = [
+    "pi-modelling.nl",
+    "modelbrouwers.nl",
+]
+
 
 class Command(BaseCommand):
     def handle(self, **options):
-        domains = extract_domains(TEST_CERT)
-        self.stdout.write("Domains: ")
-        for domain in domains:
-            self.stdout.write(f"- {domain}")
-
-        hosts = get_relevant_mx_hosts(HOST)
-        self.stdout.write(f"Relvant MX hosts for {HOST}:")
-        for host in hosts:
-            self.stdout.write(f"- {host[0]}: {host[1]}")
+        generate, domains = get_certificate_domains(TEST_CERT, MAIL_HOSTS)
+        print(generate, domains)
