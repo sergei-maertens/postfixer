f60f7ef6256ae9f1120109f58073a8a2615e70b0
Sergei Maertens <sergei@maykinmedia.nl>
2021-07-17 20:38:56 +0200

    :arrow_up: Bump dependencies (security fixes)

--- bin/compile_dependencies.sh
+++ bin/compile_dependencies.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
+#
+# Compile the dependencies for production, CI and development.
+#
+# Usage, in the root of the project:
+#
+#     ./bin/compile_dependencies.sh
+#
+# Any extra flags/arguments passed to this wrapper script are passed down to pip-compile.
+# E.g. to update a package:
+#
+#     ./bin/compile_dependencies.sh --upgrade-package django
+
 set -ex
 
 toplevel=$(git rev-parse --show-toplevel)
 
 cd $toplevel
 
-# Base deps
+export CUSTOM_COMPILE_COMMAND="./bin/compile_dependencies.sh"
+
+# Base (& prod) deps
 pip-compile \
-    --no-index \
+    --no-emit-index-url \
+    "$@" \
     requirements/base.in
 
-# Dev deps
+# Dependencies for testing
 pip-compile \
-    --no-index \
-    --output-file requirements/dev.txt\
+    --no-emit-index-url \
+    --output-file requirements/ci.txt \
+    "$@" \
     requirements/base.txt \
-    requirements/testing.in \
-    requirements/dev.in
+    requirements/test-tools.in
 
-# Jenkins/tests deps
+# Dev depedencies - exact same set as CI + some extra tooling
 pip-compile \
-    --no-index \
-    --output-file requirements/jenkins.txt \
-    requirements/base.txt \
-    requirements/testing.in \
-    requirements/jenkins.in
+    --no-emit-index-url \
+    --output-file requirements/dev.txt \
+    "$@" \
+    requirements/ci.txt \
+    requirements/dev.in
